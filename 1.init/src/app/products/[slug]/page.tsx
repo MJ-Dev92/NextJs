c50549bf35965b30f6565a4891b5bb7ee932166b
MJ-Dev92 <minjae920802@naver.com>
2024-10-15 23:59:48 +0900

    notFound

--- 1.init/src/app/products/[slug]/page.tsx
+++ 1.init/src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: {
@@ -7,7 +8,10 @@ type Props = {
 };
 
 export default function PantsPage({ params }: Props) {
-  return <h1>{params.slug}바지 제품 설명 페이지</h1>;
+  if (params.slug === "nothing") {
+    notFound();
+  }
+  return <h1>{params.slug} 제품 설명 페이지</h1>;
 }
 
 export function generateStaticParams() {
