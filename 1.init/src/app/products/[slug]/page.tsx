6c42197d9b5d0478c6919f6f5bb44163fa8e5509
MJ-Dev92 <minjae920802@naver.com>
2024-10-15 23:20:08 +0900

    generateStaticParams 함수 사용하여 페이지 미리 생성

--- 1.init/src/app/products/[slug]/page.tsx
+++ 1.init/src/app/products/[slug]/page.tsx
@@ -9,3 +9,11 @@ type Props = {
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug}바지 제품 설명 페이지</h1>;
 }
+
+export function generateStaticParams() {
+  const products = ["pants", "top", "outer"];
+
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
