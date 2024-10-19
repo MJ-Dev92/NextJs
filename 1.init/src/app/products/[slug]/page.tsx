b74352614bed13c282a2183d0bc7d07ad2bf3c87
MJ-Dev92 <minjae920802@naver.com>
2024-10-19 23:57:38 +0900

    리팩토링(설계)

--- 1.init/src/app/products/[slug]/page.tsx
+++ 1.init/src/app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { notFound } from "next/navigation";
+import { getProduct, getProducts } from "@/service/products";
 
 type Props = {
   params: {
@@ -7,15 +8,20 @@ type Props = {
   };
 };
 
-export default function PantsPage({ params }: Props) {
-  if (params.slug === "nothing") {
+export default function PantsPage({ params: { slug } }: Props) {
+  const product = getProduct(slug);
+
+  if (!product) {
     notFound();
   }
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+  return <h1>{product} 제품 설명 페이지</h1>;
 }
 
 export function generateStaticParams() {
-  const products = ["pants", "top", "outer"];
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+  const products = getProducts();
 
   return products.map((product) => ({
     slug: product,
