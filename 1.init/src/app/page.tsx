65d99fd617a99182cc7b010c17b7e17ca824f9aa
MJ-Dev92 <minjae920802@naver.com>
2024-10-19 23:35:09 +0900

    Client Server 사용해보기

--- 1.init/src/app/page.tsx
+++ 1.init/src/app/page.tsx
@@ -2,11 +2,17 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import os from "os";
+import Counter from "@/components/Counter";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   console.log("안녕");
   console.log(os.hostname());
 
-  return <h1>홈페이지 입니다!!</h1>;
+  return (
+    <>
+      <h1>홈페이지 입니다!!</h1>
+      <Counter />
+    </>
+  );
 }
