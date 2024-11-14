2545a6927c57e9e35528d345e55bf5db96a914ba
MJ-Dev92 <minjae920802@naver.com>
2024-11-14 17:08:53 +0900

    use to Image

--- 1.init/src/app/page.tsx
+++ 1.init/src/app/page.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import os from "os";
 import Counter from "@/components/Counter";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -13,6 +14,12 @@ export default function Home() {
     <>
       <h1>홈페이지 입니다!!</h1>
       <Counter />
+      <Image
+        src={"https://images.unsplash.com/photo-1441986300917-64674bd600d8"}
+        alt={"shop"}
+        width={400}
+        height={400}
+      />
     </>
   );
 }
--- 1.init/src/app/products/page.tsx
+++ 1.init/src/app/products/page.tsx
@@ -1,17 +1,18 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 import TextArticle from "../../components/TextArticle";
-
+import Image from "next/image";
+import clothesImage from "../../../public/images/clothes.jpg";
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
-  throw new Error();
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
 
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      <Image src={clothesImage} alt="Clothes" />
       <ul>
         {products.map((product, index) => (
           <li key={index}>
