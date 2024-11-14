93e9f9bdd6d825688d1637fdfb19129db98758ee
MJ-Dev92 <minjae920802@naver.com>
2024-11-14 16:48:40 +0900

    Error UI

--- 1.init/src/app/products/page.tsx
+++ 1.init/src/app/products/page.tsx
@@ -5,6 +5,7 @@ import TextArticle from "../../components/TextArticle";
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
+  throw new Error();
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
 
